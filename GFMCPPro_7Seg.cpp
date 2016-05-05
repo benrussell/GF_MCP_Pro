817be5b3983a63d1dda7fd9a6fdd8482730f0888
Ben Russell <br@x-plugins.com>
2016-05-06 08:31:26 +1000

    Fills out 7Seg module.

--- GFMCPPro_7Seg.cpp
+++ GFMCPPro_7Seg.cpp
@@ -3,3 +3,74 @@
 //
 
 #include "GFMCPPro_7Seg.h"
+
+
+GFMCPPro_7Seg::GFMCPPro_7Seg(){
+
+    _dref_crs_left      = XPDataref( (char*)GFMCPPro_7Seg::label_crs_left,   3);
+    _dref_ias_mach      = XPDataref( (char*)GFMCPPro_7Seg::label_ias_mach,   5);
+    _dref_heading       = XPDataref( (char*)GFMCPPro_7Seg::label_heading,    3);
+    _dref_altitude      = XPDataref( (char*)GFMCPPro_7Seg::label_altitude,   5);
+    _dref_vert_speed    = XPDataref( (char*)GFMCPPro_7Seg::label_vert_speed, 5);
+    _dref_crs_right     = XPDataref( (char*)GFMCPPro_7Seg::label_crs_right,  3);
+
+}
+
+
+
+void GFMCPPro_7Seg::update(){
+
+
+    unsigned char caTmp[16];
+
+
+    //CRS Left
+    GFUtils::set3f(
+            _handle,
+            GFMCPPro_7Seg::target_crs_left,
+            caTmp
+            );
+
+
+    //IAS Mach
+    GFUtils::set5f(
+            _handle,
+            GFMCPPro_7Seg::target_ias,
+            caTmp
+    );
+
+
+    //Heading
+    GFUtils::set3f(
+            _handle,
+            GFMCPPro_7Seg::target_hdg,
+            caTmp
+    );
+
+
+    //Altitude
+    GFUtils::set5f(
+            _handle,
+            GFMCPPro_7Seg::target_alt,
+            caTmp
+    );
+
+
+    //Vert Speed
+    GFUtils::set5f(
+            _handle,
+            GFMCPPro_7Seg::target_vs,
+            caTmp
+    );
+
+
+    //CRS Right
+    GFUtils::set3f(
+            _handle,
+            GFMCPPro_7Seg::target_crs_right,
+            caTmp
+    );
+
+
+
+}
--- GFMCPPro_7Seg.h
+++ GFMCPPro_7Seg.h
@@ -6,7 +6,53 @@
 #define GF_MCP_PRO_GFMCPPRO_7SEG_H
 
 
+#include <hidapi.h>
+
+
+#include "XPDataref.h"
+
+#include "GFUtils.h"
+
+
+
 class GFMCPPro_7Seg {
+public:
+    GFMCPPro_7Seg();
+    ~GFMCPPro_7Seg();
+
+    void update();
+
+    // 7 Seg byte arrays.
+    XPDataref _dref_crs_left;
+    XPDataref _dref_ias_mach;
+    XPDataref _dref_heading;
+    XPDataref _dref_altitude;
+    XPDataref _dref_vert_speed;
+    XPDataref _dref_crs_right;
+
+
+    hid_device* _handle;
+
+
+    // USB Endpoint Targets
+    unsigned char target_crs_left   = 3;
+    unsigned char target_ias        = 5;
+    unsigned char target_hdg        = 7;
+    unsigned char target_alt        = 9;
+    unsigned char target_vs         = 11;
+    unsigned char target_crs_right  = 13;
+
+
+
+    const char* label_crs_left      = "goflight/mcp_pro/7seg/crs_left";
+    const char* label_ias_mach      = "goflight/mcp_pro/7seg/ias_mach";
+    const char* label_heading       = "goflight/mcp_pro/7seg/heading";
+    const char* label_altitude      = "goflight/mcp_pro/7seg/altitude";
+    const char* label_vert_speed    = "goflight/mcp_pro/7seg/vert_speed";
+    const char* label_crs_right     = "goflight/mcp_pro/7seg/crs_right";
+
+
+
 
 };
 
