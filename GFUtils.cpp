a6dc6797447d8c0e8eeed7c0adb7e736f4aeb598
Ben Russell <br@x-plugins.com>
2016-05-05 13:58:34 +1000

    Ported a bunch of utils code from the test harness.

--- GFUtils.cpp
+++ GFUtils.cpp
@@ -3,3 +3,113 @@
 //
 
 #include "GFUtils.h"
+
+
+
+
+//USB tmp buffer
+#define buf_size 16
+
+
+
+//use this for MCP_CRS_LEFT || MCP_CRS_RIGHT || MCP_HDG
+void GFUtils::set3f( hid_device *handle, unsigned char target, int value ){
+
+    int res;
+
+    char caTmp[4];
+    snprintf( caTmp, 4, "%03d", value );
+    //printf( "set3f string:(%s)\n", caTmp);
+
+
+    unsigned char buf[buf_size];
+
+    memset( buf, 0, buf_size );
+    buf[0] = target; //MCP_CRS_LEFT;
+
+    buf[1] = gaTransTblDigits[ get_trans_index( caTmp[0] ) ];
+    buf[2] = gaTransTblDigits[ get_trans_index( caTmp[1] ) ];
+    buf[3] = gaTransTblDigits[ get_trans_index( caTmp[2] ) ];
+
+
+
+    //printf("buf: 0x%02x : %02x %02x %02x %02x %02x\n", buf[0], buf[1],buf[2],buf[3],buf[4],buf[5] );
+
+    res = hid_write( handle, buf, 8 );
+    if (res < 0)
+        printf("Unable to write..\n");
+
+
+
+}
+
+
+
+
+
+
+
+// MCP_IAS || MCP_ALT || MCP_VS
+void GFUtils::set5f( hid_device *handle, unsigned char target, int value ){
+
+    int res;
+
+    char caTmp[8];
+    snprintf( caTmp, 8, "%05d", value );
+    //printf( "set5f string:(%s)\n", caTmp);
+
+
+    unsigned char buf[buf_size];
+
+    memset( buf, 0, buf_size );
+    buf[0] = target; //MCP_CRS_LEFT;
+
+    buf[1] = gaTransTblDigits[ get_trans_index( caTmp[0] ) ];
+    buf[2] = gaTransTblDigits[ get_trans_index( caTmp[1] ) ];
+    buf[3] = gaTransTblDigits[ get_trans_index( caTmp[2] ) ];
+    buf[4] = gaTransTblDigits[ get_trans_index( caTmp[3] ) ];
+    buf[5] = gaTransTblDigits[ get_trans_index( caTmp[4] ) ];
+
+
+
+    //printf("buf: 0x%02x : %02x %02x %02x %02x %02x\n", buf[0], buf[1],buf[2],buf[3],buf[4],buf[5] );
+
+    res = hid_write( handle, buf, 8 );
+    if (res < 0)
+        printf("Unable to write..\n");
+
+
+
+}
+
+
+
+
+
+
+void GFUtils::set_leds( hid_device *handle, unsigned char *text ){
+
+    unsigned char a,b,c;
+    a = text[0];
+    b = text[1];
+    c = text[2];
+
+
+    unsigned char buf[buf_size];
+
+
+    memset( buf, 0, buf_size );
+    buf[0] = MCP_BTNS;
+
+    buf[1] = 0;
+
+    //byte ordering makes no sense relative to physical layout
+    buf[2] = a;// lower
+    buf[3] = b;// midde
+    buf[4] = c;// top row of LED's
+
+    buf[5] = 0;
+
+    hid_write( handle, buf, 16 );
+
+}
--- GFUtils.h
+++ GFUtils.h
@@ -6,7 +6,80 @@
 #define GF_MCP_PRO_GFUTILS_H
 
 
+#include <hidapi.h>
+
+#include <string.h>
+
+
+
+// USB ID tags.
+#define USB_GOFLIGHT 0x09f3
+
+// Device type
+#define USB_GOFLIGHT__MCP_PRO 0x0064
+
+
+
+// hid_write channels.
+#define MCP_CRS_LEFT 3
+#define MCP_IAS 5
+#define MCP_HDG 7
+#define MCP_ALT 9
+#define MCP_VS 11
+#define MCP_CRS_RIGHT 13
+
+#define MCP_BTNS 15
+
+
+
+
+
+#define neg_sign = 0x40;
+
+static const unsigned char gaTransTblDigits[] =
+        {
+                0x3F, // '0'
+                0x06, // '1'
+                0x5B, // '2'
+                0x4F, // '3'
+                0x66, // '4'
+                0x6D, // '5'
+                0x7D, // '6'
+                0x07, // '7'
+                0x7F, // '8'
+                0x67  // '9'
+        };
+
+
+
+
+
+unsigned char  get_trans_index( unsigned char target ){
+
+    //printf("target  in: %c/%i\n", target,target);
+
+    //translate human text into decimal data by using the ascii table offset.
+    unsigned char ret = target - 48;
+
+    //out chars may be invisible in a terminal
+    //printf("target out: %c/%i\n", ret,ret);
+
+    return ret;
+
+}
+
+
+
+
+
+
+
 class GFUtils {
+public:
+    void set3f( hid_device* handle, unsigned char target, int value );
+    void set5f( hid_device* handle, unsigned char target, int value );
+
+    void set_leds( hid_device* handle, unsigned char *text );
 
 };
 
