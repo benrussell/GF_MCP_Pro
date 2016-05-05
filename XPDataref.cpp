1205afcd26554b074102e773be2dcca0956cd2aa
Ben Russell <br@x-plugins.com>
2016-05-06 04:07:26 +1000

    Dataref callback hooks.

--- XPDataref.cpp
+++ XPDataref.cpp
@@ -14,4 +14,65 @@ XPDataref::XPDataref(char *dref_name) {
 
     XPLMDebugString("GF_MCP_Pro: Create dref..\n");
 
+    _name = std::string( dref_name );
+
+    _dref =
+            XPLMRegisterDataAccessor(
+                    dref_name, //name
+                    xplmType_Int | xplmType_Data, //type
+                    1, //writable
+
+                    XPDataref::xp_getDatai,	XPDataref::xp_setDatai,
+                    NULL, NULL, //floats
+                    NULL, NULL, //doubles
+
+                    NULL, NULL, //integer vectors
+                    NULL, NULL, //float vectors
+
+                    XPDataref::xp_getBytes, XPDataref::xp_setBytes, //byte vectors
+
+                    (void *)this, //read refcon
+                    (void *)this  //write refcon
+            );
+
+}
+
+
+int XPDataref::xp_getDatai(void *inRefcon) {
+    //get
+
+    return 0;
+}
+
+
+void XPDataref::xp_setDatai(void * inRefcon, int inValue){
+    //set
+
+    XPDataref *dref = (XPDataref*)inRefcon;
+
+
+
+
+}
+
+
+
+
+int XPDataref::xp_getBytes(
+        void* inRefcon,
+        void* outValues, /* Can be NULL */
+        int inOffset,
+        int inMax
+){
+
+    return 0;
+}
+
+void XPDataref::xp_setBytes(
+        void* inRefcon,
+        void* inValues, /* Can be NULL */
+        int inOffset,
+        int inCount
+){
+
 }
--- XPDataref.h
+++ XPDataref.h
@@ -6,10 +6,41 @@
 #define GF_MCP_PRO_XPDATAREF_H
 
 
+#include <XPLMDataAccess.h>
+
+
+#include <string>
+
+
 class XPDataref {
 public:
     XPDataref();
     XPDataref( char* dref_name );
+
+    XPLMDataRef _dref;
+
+
+    std::string _name;
+
+
+private:
+    static int xp_getDatai(void * inRefcon);
+    static void xp_setDatai(void * inRefcon, int inValue);
+
+    static int xp_getBytes(
+            void* inRefcon,
+            void* outValues, /* Can be NULL */
+            int inOffset,
+            int inMax
+    );
+    static void xp_setBytes(
+            void* inRefcon,
+            void* inValues, /* Can be NULL */
+            int inOffset,
+            int inCount
+    );
+
+
 };
 
 
