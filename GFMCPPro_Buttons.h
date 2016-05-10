0056113602017b157d4b746add9ab66948e32fbb
Ben Russell <br@x-plugins.com>
2016-05-10 14:46:40 +1000

    Tidy up of GFCommand scope.

--- GFMCPPro_Buttons.h
+++ GFMCPPro_Buttons.h
@@ -20,9 +20,7 @@ public:
 	~GFMCPPro_Buttons();
 
 
-	GFMCPPro_State* _mcp_state;
-
-
+	//read usb report and decode into various action
 	void read();
 
 	//need USB handle for read ops
@@ -38,67 +36,14 @@ public:
 
 
 
-	// ---- Buttons and Switches ----
-    // Packet 5
-    GFCommand* _btn_Speed;
-    GFCommand* _btn_LVL_CHG;
-    GFCommand* _btn_HDG_SEL;
-    GFCommand* _btn_APP;
-    GFCommand* _btn_ALT_HLD;
-    GFCommand* _btn_VS;
-    GFCommand* _btn_AP_Disengage;
-    GFCommand* _btn_FD_Right;
-
-    // Packet 6
-    GFCommand* _btn_ALT_INV;
-    GFCommand* _btn_CWS_A;
-    GFCommand* _btn_CWS_B;
-    GFCommand* _btn_IAS_Dial;
-    GFCommand* _btn_HDG_Dial;
-    GFCommand* _btn_ALT_Dial;
-    GFCommand* _btn_FD_Left;
-    GFCommand* _btn_N1;
-
-    // Packet 7
-    GFCommand* _btn_VNAV;
-    GFCommand* _btn_LNAV;
-    GFCommand* _btn_CMD_A;
-    GFCommand* _btn_CMD_B;
-    GFCommand* _btn_AT_Arm;
-    GFCommand* _btn_CO;
-    GFCommand* _btn_SPD_INTV;
-    GFCommand* _btn_VOR_LOC;
-
-
-
-    // ---- Scroll Wheels and Rotaries ----
-    GFCommand* _btn_crs_left_inc;
-    GFCommand* _btn_crs_left_dec;
-
-    GFCommand* _btn_ias_mach_inc;
-    GFCommand* _btn_ias_mach_dec;
-
-    GFCommand* _btn_heading_inc;
-    GFCommand* _btn_heading_dec;
-
-    GFCommand* _btn_altitude_inc;
-    GFCommand* _btn_altitude_dec;
-
-    GFCommand* _btn_vert_speed_inc;
-    GFCommand* _btn_vert_speed_dec;
-
-    GFCommand* _btn_crs_right_inc;
-    GFCommand* _btn_crs_right_dec;
-    // -----------------------------------
-
-
-
-
-
 
 private:
 
-    void _read_usb();
+	GFMCPPro_State* _mcp_state;
+
+
+
+	void _read_usb();
 
     void _proc_buttons( int res, unsigned char* buf );
 
@@ -106,7 +51,67 @@ private:
 
 
 
-    const char* _label_no_description = "No description.";
+
+	// ---- Buttons and Switches ----
+	// Packet 5
+	GFCommand* _btn_Speed;
+	GFCommand* _btn_LVL_CHG;
+	GFCommand* _btn_HDG_SEL;
+	GFCommand* _btn_APP;
+	GFCommand* _btn_ALT_HLD;
+	GFCommand* _btn_VS;
+	GFCommand* _btn_AP_Disengage;
+	GFCommand* _btn_FD_Right;
+
+	// Packet 6
+	GFCommand* _btn_ALT_INV;
+	GFCommand* _btn_CWS_A;
+	GFCommand* _btn_CWS_B;
+	GFCommand* _btn_IAS_Dial;
+	GFCommand* _btn_HDG_Dial;
+	GFCommand* _btn_ALT_Dial;
+	GFCommand* _btn_FD_Left;
+	GFCommand* _btn_N1;
+
+	// Packet 7
+	GFCommand* _btn_VNAV;
+	GFCommand* _btn_LNAV;
+	GFCommand* _btn_CMD_A;
+	GFCommand* _btn_CMD_B;
+	GFCommand* _btn_AT_Arm;
+	GFCommand* _btn_CO;
+	GFCommand* _btn_SPD_INTV;
+	GFCommand* _btn_VOR_LOC;
+
+
+
+	// ---- Scroll Wheels and Rotaries ----
+	GFCommand* _btn_crs_left_inc;
+	GFCommand* _btn_crs_left_dec;
+
+	GFCommand* _btn_ias_mach_inc;
+	GFCommand* _btn_ias_mach_dec;
+
+	GFCommand* _btn_heading_inc;
+	GFCommand* _btn_heading_dec;
+
+	GFCommand* _btn_altitude_inc;
+	GFCommand* _btn_altitude_dec;
+
+	GFCommand* _btn_vert_speed_inc;
+	GFCommand* _btn_vert_speed_dec;
+
+	GFCommand* _btn_crs_right_inc;
+	GFCommand* _btn_crs_right_dec;
+	// -----------------------------------
+
+
+
+
+
+
+
+	const char* _label_no_description = "No description.";
 
 
     const char* _label_btn_speed        = "goflight/mcp_pro/emit/btn_speed";
