1ed72aff7603d7cbd79b4e0e988975ceeb8cb4d8
Ben Russell <br@x-plugins.com>
2016-05-19 01:05:46 +1000

    Refactor GFCommand for clarity. Culls useless code.

--- src/GFCommand.cpp
+++ src/GFCommand.cpp
@@ -23,10 +23,10 @@ GFCommand::GFCommand( const std::string cmd_name, const std::string cmd_descript
     _name = cmd_name;
     //_description = cmd_description;
 
-    _cmd = XPLMCreateCommand( cmd_name.c_str(), cmd_description.c_str() );
+    _xpcmd = XPLMCreateCommand( cmd_name.c_str(), cmd_description.c_str() );
 
 
-    XPLMRegisterCommandHandler( _cmd,              // in Command name
+    XPLMRegisterCommandHandler( _xpcmd,              // in Command name
                                 GFMCPPro_cmdHandler,       // in Handler
                                 1,                      // Receive input before plugin windows.
                                 (void *)this
@@ -36,33 +36,18 @@ GFCommand::GFCommand( const std::string cmd_name, const std::string cmd_descript
 
 
 void GFCommand::Begin() {
-    XPLMCommandBegin( _cmd );
+    XPLMCommandBegin( _xpcmd );
     _cmd_active = 1;
 }
 
 
 void GFCommand::Stop() {
     if( _cmd_active ) {
-        XPLMCommandEnd(_cmd);
+        XPLMCommandEnd( _xpcmd );
         _cmd_active = 0;
     }
 }
 
-
-
-// --- Action handlers ---
-
-
-// what do we actually do when this item is running.
-void GFCommand::_Action_Start(){
-	XPLMDebugString("GFCommand: _Action_Start() default.\n");
-}
-void GFCommand::_Action_Hold(){
-	XPLMDebugString("GFCommand: _Action_Hold() default.\n");
-}
-void GFCommand::_Action_Stop(){
-	XPLMDebugString("GFCommand: _Action_Stop() default.\n");
-}
 
 
 // eof
--- src/GFCommand.h
+++ src/GFCommand.h
@@ -14,23 +14,23 @@
 
 class GFCommand {
 private:
-    XPLMCommandRef _cmd;
     int _cmd_active; //held high while the hardware button is down, returns to low when we get a release event from USB HID
+
+	XPLMCommandRef _xpcmd;
+
 
 public:
     GFCommand( const std::string cmd_name, const std::string cmd_description, void* _button_router );
 
     std::string _name;
-    //std::string _description;
+
+
+	//std::string _description;
 
 
     void Begin();
     void Stop();
 
-	// what do we actually do when this item is running.
-	virtual void _Action_Start();
-	virtual void _Action_Hold();
-	virtual void _Action_Stop();
 
 
 	void* _button_router;
