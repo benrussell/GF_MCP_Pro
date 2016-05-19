cadd4d02d3d1d0f48856ff45b9922a9a1134f7fc
Ben Russell <br@x-plugins.com>
2016-05-19 15:15:24 +1000

    Optimize GFUtils. Ensures correct buffer sizes for all IO ops.

--- src/GFUtils.cpp
+++ src/GFUtils.cpp
@@ -22,28 +22,22 @@ void GFUtils::set3f( hid_device *handle, unsigned char target, unsigned char *va
 		return;
 	}
 
-
     unsigned char buf[GF_usb_buffer_size];
-
     memset( buf, 0, GF_usb_buffer_size );
-    buf[0] = target; //MCP_CRS_LEFT;
 
+    buf[0] = target; //MCP_CRS_LEFT;
     buf[1] = GFUtils::translateCharTo7Seg( value[0] );
     buf[2] = GFUtils::translateCharTo7Seg( value[1] );
     buf[3] = GFUtils::translateCharTo7Seg( value[2] );
 
     //printf("buf: 0x%02x : %02x %02x %02x %02x %02x\n", buf[0], buf[1],buf[2],buf[3],buf[4],buf[5] );
 
-    int res = hid_write( handle, buf, 8 );
+    int res = hid_write( handle, buf, GF_usb_buffer_size );
     if (res < 0) {
 		GFUtils::Log("GFUtils::set3f(..) : Unable to write 3f..\n"); //FIXME: usb write fail, propagate
 	}
 
 }
-
-
-
-
 
 
 
@@ -55,12 +49,10 @@ void GFUtils::set5f( hid_device *handle, unsigned char target, unsigned char *va
 		return;
 	}
 
-
     unsigned char buf[GF_usb_buffer_size];
-
     memset( buf, 0, GF_usb_buffer_size );
-    buf[0] = target; //MCP_CRS_LEFT;
 
+    buf[0] = target; //MCP_CRS_LEFT;
     buf[1] = GFUtils::translateCharTo7Seg( value[0] );
     buf[2] = GFUtils::translateCharTo7Seg( value[1] );
     buf[3] = GFUtils::translateCharTo7Seg( value[2] );
@@ -69,15 +61,12 @@ void GFUtils::set5f( hid_device *handle, unsigned char target, unsigned char *va
 
     //printf("buf: 0x%02x : %02x %02x %02x %02x %02x\n", buf[0], buf[1],buf[2],buf[3],buf[4],buf[5] );
 
-	int res = hid_write( handle, buf, 8 );
+	int res = hid_write( handle, buf, GF_usb_buffer_size );
 	if (res < 0) {
 		GFUtils::Log("GFUtils::set5f(..) : Unable to write 5f..\n"); //FIXME: usb write fail, propagate
 	}
 
 }
-
-
-
 
 
 
@@ -88,37 +77,22 @@ void GFUtils::set_leds( hid_device *handle, unsigned char target, unsigned char 
 		return;
 	}
 
-
-    unsigned char a,b,c;
-    a = values[0];
-    b = values[1];
-    c = values[2];
-
-
     unsigned char buf[GF_usb_buffer_size];
     memset( buf, 0, GF_usb_buffer_size );
 
-    buf[0] = target;
-    buf[1] = 0;
+	//byte ordering makes no sense relative to physical layout
+	buf[0] = target;
+    buf[1] = 0; //not used
+    buf[2] = values[0]; // lower
+    buf[3] = values[1]; // midde
+    buf[4] = values[2]; // top row of LED's
 
-    //byte ordering makes no sense relative to physical layout
-    buf[2] = a; // lower
-    buf[3] = b; // midde
-    buf[4] = c; // top row of LED's
-
-    buf[5] = 0;
-
-
-    int res = hid_write( handle, buf, 16 );
+    int res = hid_write( handle, buf, GF_usb_buffer_size ); //FIXME: why was this 16?
 	if (res < 0) {
 		GFUtils::Log("GFUtils::set_leds(..) : Unable to write packet..\n"); //FIXME: usb write fail, propagate
 	}
 
 }
-
-
-
-
 
 
 
@@ -160,10 +134,8 @@ unsigned char GFUtils::translateCharTo7Seg( unsigned char raw ) {
     } //switch(..)
 
 
-
     return  ret;
-
-
+	
 } //unsigned char GFUtils::translateCharTo7Seg( unsigned char raw )
 
 
